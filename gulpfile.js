e3defe6027c19b0c94e6f461ccb8adede25cd841
marcelschulze <ms88.privat@gmail.com>
2015-02-23 00:18:43 +0100

    init ngdocs

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,51 @@
 
 var gulp = require('gulp');
 var karma = require('karma').server;
-
+var historyApiFallback = require('connect-history-api-fallback');
 var $ = require('gulp-load-plugins')();
 
+gulp.task('watch', ['scripts', 'test', 'doc'], function() {
+	gulp.watch('src/**/*.js', ['scripts', 'test', 'doc']);
+	gulp.watch('tests/**/*Spec.js', ['test']);
+	gulp.watch('docs/index.html', ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
 
 
+/* =============================================================================== */
+/* 
+DOCUMENTATION (neeeds server to be viewed)  
+*/
+/* =============================================================================== */
+gulp.task('connect', function() {
+	return $.connect.server({
+		root: 'docs',
+		port: 8888,
+		livereload: true,
+		middleware: function() { // params: connect, opt
+			return [ historyApiFallback ];
+		}
+	});
+});
+ 
+gulp.task('html', function () {
+	gulp.src('./docs/index.html')
+		.pipe($.connect.reload());
+});
+ 
+gulp.task('doc', function() {
+	return gulp.src('src/**/*.js')
+	    .pipe($.ngdocs.process())
+	    .pipe(gulp.dest('./docs'));
+});
+
+
+/* =============================================================================== */
+/* 
+SCRIPTS (compile Babel to es5)
+*/
+/* =============================================================================== */
 gulp.task('scripts', [], function() {
 	return gulp.src('src/**/*.js')
 		.pipe($.babel())
@@ -15,15 +55,14 @@ gulp.task('scripts', [], function() {
 		;
 });
 
-gulp.task('watch', ['scripts', 'test'], function() {
 
-	gulp.watch('src/**/*.js', ['scripts', 'test']);
-	gulp.watch('tests/**/*Spec.js', ['test']);
-});
+/* =============================================================================== */
+/* 
+UNIT TESTING  
+*/
+/* =============================================================================== */
 
-/**
- * Run test once and exit
- */
+// Run test once and exit
 gulp.task('test', ['scripts'], function (done) {
 	karma.start({
 		configFile: __dirname + '/karma.conf.js',
@@ -33,9 +72,7 @@ gulp.task('test', ['scripts'], function (done) {
 	});
 });
 
-/**
- * Watch for file changes and re-run tests on each change
- */
+// Watch for file changes and re-run tests on each change
 gulp.task('tdd', ['scripts'], function(done) {
 	karma.start({
 		configFile: __dirname + '/karma.conf.js'
@@ -44,4 +81,5 @@ gulp.task('tdd', ['scripts'], function(done) {
 	});
 });
 
-gulp.task('default', ['watch']);
+
+
--- src/stuffmanager.js
+++ src/stuffmanager.js
@@ -1,20 +1,56 @@
+/* global angular */
 
 
 (function(angular) {
 	'use strict';
 
+	 /**
+     * @ngdoc overview
+     * @name stuffmanager
+     * @description Manage your Stuff better!
+     */
 	angular
 		.module('stuffmanager', [])
+		/**
+	     * @ngdoc service
+	     * @name stuffmanager.facilityManager
+	     * @description facilityManager manages your localData you need to save between page reloads
+	     */
 		.factory('facilityManager', function() {
 
+			/**
+		     * @ngdoc object
+		     * @name FacilityInstance
+		     * @description FacilityInstance
+		     */
 			class Facility {
 				constructor(name = 'defaultName', {cache = true} = {}) {
 					this.name = name;
 					this.cache = cache;
 				}
+
+				/**
+                 * @ngdoc method
+                 * @name get
+                 * @description get data by id
+                 * @methodOf FacilityInstance
+                 * @param {Number} id must be an integer
+                 * @returns {Object | Array} of data
+                 */
+				get(id) {
+					return 'Hello world' + id;
+				}
 			}
 
+			
 			return {
+				 /**
+                 * @ngdoc method
+                 * @name create
+                 * @methodOf stuffmanager.facilityManager
+                 * @param {string} name Name of the instance
+                 * @returns {INSTANCE} Instance of Facility Class
+                 */
 				create: function(name) {
 					return new Facility(name);
 				},
@@ -24,3 +60,6 @@
 
 
 }(window.angular));
+
+
+/* EOF */
